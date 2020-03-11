1044125e55f8e2d7d560d0e86ddfebf0d78b4a5a
ShanghuiXu49 <xushanghui49@outlook.com>
2020-03-11 10:37:22 -0400

    Add the getPOI function to geoJSON.js file

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var pg = require('pg');
-      var geoJSON = require('express').Router();
+var geoJSON = require('express').Router();
 var fs = require('fs');
 
 var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
@@ -20,3 +20,20 @@ geoJSON.route('/testGeoJSON').get(function (req,res) {
 });
 
 module.exports = geoJSON;
+
+geoJSON.get('/postgistest', function (req,res) {
+pool.connect(function(err,client,done) {
+       if(err){
+           console.log("not able to get connection "+ err);
+           res.status(400).send(err);
+       } 
+       client.query('SELECT name FROM london_counties' ,function(err,result) {
+           done(); 
+           if(err){
+               console.log(err);
+               res.status(400).send(err);
+           }
+           res.status(200).send(result.rows);
+       });
+    });
+});
