281fe9a5b822b701cb660d0cf16d28318d9bfaca
ShanghuiXu49 <xushanghui49@outlook.com>
2020-03-22 09:58:54 -0400

    Fix the bug that can not connect to crud.js file

--- dataAPI.js
+++ dataAPI.js
@@ -1,46 +1,47 @@
-// express is the server that forms part of the nodejs program 
-var express = require('express'); 
-var path = require("path"); 
-var app = express(); 
-var fs = require('fs'); 
+// express is the server that forms part of the nodejs program
+var express = require('express');
+var path = require("path");
+var fs = require('fs');
+var app = express();
+const bodyParser = require('body-parser');
 
 // add an https server to serve files 
-var https = require('https'); 
+var https = require('https');
 
-var privateKey = fs.readFileSync('/home/studentuser/certs/cert.key').toString(); 
-var certificate = fs.readFileSync('/home/studentuser/certs/cert.crt').toString(); 
+var privateKey = fs.readFileSync('/home/studentuser/certs/cert.key').toString();
+var certificate = fs.readFileSync('/home/studentuser/certs/cert.crt').toString();  
 
-var credentials = {key: privateKey, cert: certificate}; 
-var httpsServer = https.createServer(credentials, app); 
+var credentials = {key: privateKey, cert: certificate};
+var httpsServer = https.createServer(credentials, app);
 
-httpsServer.listen(4480); 
-const bodyParser = require('body-parser');
+httpsServer.listen(4480);
 app.use(bodyParser.urlencoded({ extended: true }));
 
 
-app.get('/',function (req,res) { 
-        res.send("hello world from the Data API"); 
+app.get('/',function (req,res) {
+	res.send("Hello World from the demo HTTPS Server on port 8000");
 });
 
-// adding functionality to allow cross-origin queries when PhoneGap is running a server
+// adding functionality to log the requests
 app.use(function (req, res, next) {
 	var filename = path.basename(req.url);
 	var extension = path.extname(filename);
 	console.log("The file " + filename + " was requested.");
 	next();
-})
+});
 
-// adding functionality to log the requests
+// adding CORS
 app.use(function(req, res, next) {
   res.header("Access-Control-Allow-Origin", "*"); // update to match the domain you will make the request from
   res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
   next();
 });
 
-const geoJSON = require('./routes/geoJSON'); 
+
+const geoJSON = require('./routes/geoJSON');
 app.use('/', geoJSON);
 
-const crud = require('./routes/crud');
+const crud = require('./routes/crud'); 
 app.use('/', crud);
 
 app.use(express.static(__dirname));
--- routes/crud.js
+++ routes/crud.js
@@ -1,22 +1,24 @@
-    var express = require('express');
-    var pg = require('pg');
-	var crud = require('express').Router();
-    var fs = require('fs');
-    var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
+var express = require('express'); 
+var pg = require('pg'); 
+var crud = require('express').Router(); 
+var fs = require('fs'); 
+var configtext = ""+fs.readFileSync("/home/studentuser/certs/postGISConnection.js");
 
-    // now convert the configruation file into the correct format -i.e. a name/value pair array
-    var configarray = configtext.split(",");
-    var config = {};
-    for (var i = 0; i < configarray.length; i++) {
-        var split = configarray[i].split(':');
-        config[split[0].trim()] = split[1].trim();
-    }
-    var pool = new pg.Pool(config);
-    console.log(config);
+// now convert the configruation file into the correct format -i.e. a name/value pair array 
+var configarray = configtext.split(","); 
+var config = {}; 
+for (var i = 0; i < configarray.length; i++) { 
+    var split = configarray[i].split(':'); 
+    config[split[0].trim()] = split[1].trim(); 
+}
 
-    crud.route('/testCRUD').get(function (req,res) {
-        res.json({message:req.originalUrl});
-    });
+var pool = new pg.Pool(config); 
+console.log(config);
+
+crud.route('/testCRUD').get(function (req,res) { 
+    res.json({message:req.originalUrl}); 
+});
+
 
     crud.post('/insertFormData',(req,res) => {
             console.dir(req.body);
@@ -81,5 +83,4 @@ crud.post('/deleteFormData',(req,res) => {
       });
 });
 
-
-    module.exports = crud;
+module.exports = crud;
