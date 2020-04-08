d2ab6e3e4be0c6308c5d90a42ac72c8650355817
ShanghuiXu49 <xushanghui49@outlook.com>
2020-04-08 10:45:37 -0400

    Add the SQL to get the unanswered questions from database

--- routes/geoJSON.js
+++ routes/geoJSON.js
@@ -438,4 +438,76 @@ geoJSON.get('/getDifficultQuestions', function (req,res) {
 
 })
 
+//code to get the last 5 question that the user answered
+geoJSON.get('/getLast5Question/:port_id', function (req,res) {
+     pool.connect(function(err,client,done) {
+        if(err){
+            console.log("not able to get connection "+ err);
+            res.status(400).send(err);
+        }
+          var colnames = "id, question_title, question_text, answer_1,";
+          colnames = colnames + "answer_2, answer_3, answer_4, port_id, correct_answer";
+          console.log("colnames are " + colnames);
+
+          // now use the inbuilt geoJSON functionality
+          // and create the required geoJSON format using a query adapted from here:
+          // http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+          // note that query needs to be a single string with no line breaks so built it up bit by bit
+         var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+          querystring += "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.location)::json As geometry, ";
+          querystring += "row_to_json((SELECT l FROM (SELECT id, question_title, question_text, answer_1, answer_2, answer_3, answer_4, port_id, correct_answer, answer_correct) As l      )) As properties";
+          querystring += " FROM (select a.*, b.answer_correct from public.quizquestions a inner join ";
+      querystring += " (select question_id, answer_selected=correct_answer as answer_correct from public.quizanswers";
+      querystring += " where port_id = $1 order by timestamp desc limit 5) b on a.id = b.question_id) as lg) As f";
+          console.log(querystring);
+          var port_id = req.params.port_id; //
+          // run the second query
+          client.query(querystring,[port_id],function(err,result){
+            //call `done()` to release the client back to the pool
+            done();
+            if(err){
+                  console.log(err);
+                  res.status(400).send(err);
+             }
+            res.status(200).send(result.rows);
+        });
+    });
+
+});
+
+geoJSON.get('/getUnanswered/:port_id', function (req,res) {
+     pool.connect(function(err,client,done) {
+        if(err){
+            console.log("not able to get connection "+ err);
+            res.status(400).send(err);
+        }
+          var colnames = "id, question_title, question_text, answer_1,";
+          colnames = colnames + "answer_2, answer_3, answer_4, port_id, correct_answer";
+          console.log("colnames are " + colnames);
+          var param1 = req.params.id;
+
+          // now use the inbuilt geoJSON functionality
+          // and create the required geoJSON format using a query adapted from here:
+          // http://www.postgresonline.com/journal/archives/267-Creating-GeoJSON-Feature-Collections-with-JSON-and-PostGIS-functions.html, accessed 4th January 2018
+          // note that query needs to be a single string with no line breaks so built it up bit by bit
+         var querystring = " SELECT 'FeatureCollection' As type, array_to_json(array_agg(f)) As features  FROM ";
+          querystring += "(SELECT 'Feature' As type     , ST_AsGeoJSON(lg.location)::json As geometry, ";
+          querystring += "row_to_json((SELECT l FROM (SELECT id, question_title, question_text, answer_1, answer_2, answer_3, answer_4, port_id, correct_answer) As l      )) As properties";
+          querystring += "  FROM (select * from public.quizquestions where id in (select question_id from public.quizanswers where port_id = $1 and answer_selected <> correct_answer union all select id from public.quizquestions where id not in (select question_id from public.quizanswers) and port_id = $2) ) as lg) As f";
+          console.log(querystring);
+          var port_id = req.params.port_id; //
+          // run the second query
+          client.query(querystring,[param1,port_id],function(err,result){
+            //call `done()` to release the client back to the pool
+            done();
+            if(err){
+                  console.log(err);
+                  res.status(400).send(err);
+             }
+            res.status(200).send(result.rows);
+        });
+    });
+
+})
+
     module.exports = geoJSON;
